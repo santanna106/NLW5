b02cdddd874842f7310db4ea11bf88353c665d18
Gabriel Andrade <gabriel.santana@gmail.com>
2021-04-25 08:29:21 -0300

    Initial commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,33 @@
+import {useState} from 'react';
+
+import { Header } from '../components/Header';
+import { Player } from '../components/Player';
+
+import styles from '../styles/app.module.scss';
+
+import {PlayerContextProvider} from '../contexts/PlayerContext';
+
+import '../styles/global.scss';
+
+
+
+
+function MyApp({ Component, pageProps }) {
+
+  return (
+    <PlayerContextProvider>
+
+      <div className={styles.wrapper}>
+          <main>
+            <Header/>
+            <Component {...pageProps}/>
+
+          </main>
+          <Player/>
+        </div>
+    </PlayerContextProvider>
+      
+     )
+}
+
+export default MyApp
